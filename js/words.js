6b169c0f26fb29a9f794ce85dc0bc968f75985bf
bjorn <156990105+xiongshuang@users.noreply.github.com>
2025-01-13 14:12:54 -0600

    hi

---
+++ js/words.js
@@ -0,0 +1,1097 @@
+let words = [
+    {
+        word: "Abbot Cat",
+      },
+      {
+        word: "Aberailian Tropical Flower",
+      },
+      {
+        word: "Abyssinian",
+      },
+      {
+        word: "Abyssinian Bobtail",
+      },
+      {
+        word: "Adirondack Longhair",
+      },
+      {
+        word: "Aebon Cat",
+      },
+      {
+        word: "Alicocay",
+      },
+      {
+        word: "Amarapura",
+      },
+      {
+        word: "AmberLynn",
+      },
+      {
+        word: "American Bobtail",
+      },
+      {
+        word: "American Curl",
+      },
+      {
+        word: "American Longhair",
+      },
+      {
+        word: "American Lynx",
+      },
+      {
+        word: "American Mink",
+      },
+      {
+        word: "American Shorthair",
+      },
+      {
+        word: "American Wirehair",
+      },
+      {
+        word: "Andromeda",
+      },
+      {
+        word: "Andromeda Moon",
+      },
+      {
+        word: "Anubis",
+      },
+      {
+        word: "Apollo Sunrise",
+      },
+      {
+        word: "Arabian Shorthair",
+      },
+      {
+        word: "Arboretum Mist",
+      },
+      {
+        word: "Arctic Cat",
+      },
+      {
+        word: "Arctic Snowcat",
+      },
+      {
+        word: "Ashmarian",
+      },
+      {
+        word: "Athena Cat",
+      },
+      {
+        word: "Aurilia",
+      },
+      {
+        word: "Aurinkose",
+      },
+      {
+        word: "Aurora",
+      },
+      {
+        word: "Aurora Fold",
+      },
+      {
+        word: "Australian Mist",
+      },
+      {
+        word: "Autumn Wonderland",
+      },
+      {
+        word: "Avrohal",
+      },
+      {
+        word: "Balinese",
+      },
+      {
+        word: "Bastet Cat",
+      },
+      {
+        word: "Beast of Boncath",
+      },
+      {
+        word: "Beauté D'or",
+      },
+      {
+        word: "Bengal",
+      },
+      {
+        word: "Big Ben",
+      },
+      {
+        word: "Bijou Petit",
+      },
+      {
+        word: "Birman",
+      },
+      {
+        word: "Black Mountain",
+      },
+      {
+        word: "Black Rose",
+      },
+      {
+        word: "Blaze of Glory",
+      },
+      {
+        word: "Blue Moon",
+      },
+      {
+        word: "Blue Oasis",
+      },
+      {
+        word: "Blue Ocean",
+      },
+      {
+        word: "Bobcoon",
+      },
+      {
+        word: "Bohemian Rhapsody",
+      },
+      {
+        word: "BokuMo",
+      },
+      {
+        word: "Bombay",
+      },
+      {
+        word: "Briar Rose",
+      },
+      {
+        word: "British Longhair",
+      },
+      {
+        word: "British Shorthair",
+      },
+      {
+        word: "Bulgarian Angora",
+      },
+      {
+        word: "Burmese",
+      },
+      {
+        word: "Burmilla",
+      },
+      {
+        word: "ButaCat",
+      },
+      {
+        word: "California Spangled",
+      },
+      {
+        word: "Cander Kitten",
+      },
+      {
+        word: "Caprican Coon",
+      },
+      {
+        word: "Caribbean Longhair",
+      },
+      {
+        word: "Caribbean Shorthair",
+      },
+      {
+        word: "Cats of Artemis",
+      },
+      {
+        word: "Cayuga Ruffle",
+      },
+      {
+        word: "Celtic Frost",
+      },
+      {
+        word: "Celtic Rough",
+      },
+      {
+        word: "Ceteria",
+      },
+      {
+        word: "Ceylon",
+      },
+      {
+        word: "Ch'in",
+      },
+      {
+        word: "Chanceux",
+      },
+      {
+        word: "Charleston Red",
+      },
+      {
+        word: "Chartreux",
+      },
+      {
+        word: "Chat de Bavier",
+      },
+      {
+        word: "Chatière",
+      },
+      {
+        word: "Chiisai",
+      },
+      {
+        word: "Chite",
+      },
+      {
+        word: "Chocolate Norwegian",
+      },
+      {
+        word: "Chocolate Odd-ear",
+      },
+      {
+        word: "Chumbles",
+      },
+      {
+        word: "Cindlemond",
+      },
+      {
+        word: "Cinnafawn Rag",
+      },
+      {
+        word: "City Watch Cat",
+      },
+      {
+        word: "Clarissa Cat",
+      },
+      {
+        word: "Classique",
+      },
+      {
+        word: "Colorpoint Mau",
+      },
+      {
+        word: "Colorpoint Somali",
+      },
+      {
+        word: "Cornish Rex",
+      },
+      {
+        word: "Curled Almond",
+      },
+      {
+        word: "Curled Coon",
+      },
+      {
+        word: "Curlicue",
+      },
+      {
+        word: "Cymric",
+      },
+      {
+        word: "D'éria",
+      },
+      {
+        word: "Damianos",
+      },
+      {
+        word: "Dane Cat",
+      },
+      {
+        word: "Dazzle Cat",
+      },
+      {
+        word: "Devon Rex",
+      },
+      {
+        word: "Dhol Chat",
+      },
+      {
+        word: "Diaga-Arakat",
+      },
+      {
+        word: "Dianoetikos Rex",
+      },
+      {
+        word: "Dipper",
+      },
+      {
+        word: "Diva Cat",
+      },
+      {
+        word: "Diwata",
+      },
+      {
+        word: "Djin",
+      },
+      {
+        word: "Dracat",
+      },
+      {
+        word: "Dragoste",
+      },
+      {
+        word: "Débardeurs' Mouser",
+      },
+      {
+        word: "Egyptian Mau",
+      },
+      {
+        word: "Egyptian Twilight",
+      },
+      {
+        word: "Equinoxis",
+      },
+      {
+        word: "Esjotic",
+      },
+      {
+        word: "Eurasian",
+      },
+      {
+        word: "European Delacroix",
+      },
+      {
+        word: "European Dilute",
+      },
+      {
+        word: "European Longhair",
+      },
+      {
+        word: "European Masquerade",
+      },
+      {
+        word: "European Shorthair",
+      },
+      {
+        word: "European Wirehair",
+      },
+      {
+        word: "Excalibur",
+      },
+      {
+        word: "Exotic Shorthair",
+      },
+      {
+        word: "Ezra",
+      },
+      {
+        word: "Fairy Tiger",
+      },
+      {
+        word: "Flamedance",
+      },
+      {
+        word: "Flemish Fold",
+      },
+      {
+        word: "Folded Ridge",
+      },
+      {
+        word: "Forbidden Fold",
+      },
+      {
+        word: "Foreign Perm",
+      },
+      {
+        word: "Franperley-ley",
+      },
+      {
+        word: "Gaelic Song",
+      },
+      {
+        word: "Geneva",
+      },
+      {
+        word: "Georgia Forest Cat",
+      },
+      {
+        word: "German Rex",
+      },
+      {
+        word: "German Wirehair",
+      },
+      {
+        word: "Golden Doll",
+      },
+      {
+        word: "Golden Lynx",
+      },
+      {
+        word: "Gonzales Kitty",
+      },
+      {
+        word: "Hamidin",
+      },
+      {
+        word: "Havana Brown",
+      },
+      {
+        word: "Highland Fold",
+      },
+      {
+        word: "Himalayan Bear Cat",
+      },
+      {
+        word: "Hindustani",
+      },
+      {
+        word: "Hiraeth",
+      },
+      {
+        word: "Ifrit",
+      },
+      {
+        word: "Indy Cat",
+      },
+      {
+        word: "Infinity",
+      },
+      {
+        word: "Iomaera",
+      },
+      {
+        word: "Ipsum",
+      },
+      {
+        word: "Irish Curl",
+      },
+      {
+        word: "Iuvenesco",
+      },
+      {
+        word: "Jade Panther",
+      },
+      {
+        word: "Japanese Bobtail",
+      },
+      {
+        word: "Japanese Lynx",
+      },
+      {
+        word: "Jikoku",
+      },
+      {
+        word: "Joshrex",
+      },
+      {
+        word: "Kamet Shortnose",
+      },
+      {
+        word: "Kanaani",
+      },
+      {
+        word: "Kentish",
+      },
+      {
+        word: "Keuda",
+      },
+      {
+        word: "Korat",
+      },
+      {
+        word: "Krumben",
+      },
+      {
+        word: "Kurilian Bobtail",
+      },
+      {
+        word: "Kutta Billi",
+      },
+      {
+        word: "Kyasha",
+      },
+      {
+        word: "LaPerm",
+      },
+      {
+        word: "Laguna",
+      },
+      {
+        word: "Lanikai",
+      },
+      {
+        word: "Larex",
+      },
+      {
+        word: "Leo",
+      },
+      {
+        word: "Libra",
+      },
+      {
+        word: "Liebee",
+      },
+      {
+        word: "Lilac Norwegian",
+      },
+      {
+        word: "Lilith",
+      },
+      {
+        word: "Lindernord",
+      },
+      {
+        word: "Long-haired Australian Mist",
+      },
+      {
+        word: "Longhair German Rex",
+      },
+      {
+        word: "Lorem",
+      },
+      {
+        word: "Lorem Longhair",
+      },
+      {
+        word: "Lotis Cat",
+      },
+      {
+        word: "Luna Even",
+      },
+      {
+        word: "Lunaris",
+      },
+      {
+        word: "Lynahnx",
+      },
+      {
+        word: "Maine Coon",
+      },
+      {
+        word: "Majestic Coon",
+      },
+      {
+        word: "Maltesian",
+      },
+      {
+        word: "Mandalay",
+      },
+      {
+        word: "Manx",
+      },
+      {
+        word: "Masked Enigma",
+      },
+      {
+        word: "MauiCat",
+      },
+      {
+        word: "Meceena Cat",
+      },
+      {
+        word: "Mediterranean Harbor Cat",
+      },
+      {
+        word: "Mei Mao",
+      },
+      {
+        word: "Mennym",
+      },
+      {
+        word: "Mexican Forest Cat",
+      },
+      {
+        word: "Midas",
+      },
+      {
+        word: "Minbarie",
+      },
+      {
+        word: "Mini British",
+      },
+      {
+        word: "Mini Curl",
+      },
+      {
+        word: "Miniature Jaguar",
+      },
+      {
+        word: "Miniature Leopard",
+      },
+      {
+        word: "Miniature Tiger",
+      },
+      {
+        word: "Minipuff",
+      },
+      {
+        word: "Minskin",
+      },
+      {
+        word: "Mithul",
+      },
+      {
+        word: "Mogle Cat",
+      },
+      {
+        word: "Mongolian Mini",
+      },
+      {
+        word: "Moon Dreamer",
+      },
+      {
+        word: "Moondust",
+      },
+      {
+        word: "Morgan Mini",
+      },
+      {
+        word: "Morning Star",
+      },
+      {
+        word: "Munchkin",
+      },
+      {
+        word: "Myanmar",
+      },
+      {
+        word: "Nanokitty",
+      },
+      {
+        word: "Navarroise",
+      },
+      {
+        word: "Neapolitan",
+      },
+      {
+        word: "Nebelung",
+      },
+      {
+        word: "Neva Masquarade",
+      },
+      {
+        word: "Noctua",
+      },
+      {
+        word: "Northern Fluff",
+      },
+      {
+        word: "Northern Light",
+      },
+      {
+        word: "Norwegian Forestcat",
+      },
+      {
+        word: "Norwegian Lynx",
+      },
+      {
+        word: "Norwegian Ottercat",
+      },
+      {
+        word: "Norwegian Shorthair",
+      },
+      {
+        word: "Norwegian Yggdrasil",
+      },
+      {
+        word: "Oceanic",
+      },
+      {
+        word: "Ocicat",
+      },
+      {
+        word: "Olympus River Cat",
+      },
+      {
+        word: "Optical",
+      },
+      {
+        word: "Orchid Cat",
+      },
+      {
+        word: "Oriental Bicolor",
+      },
+      {
+        word: "Oriental Longhair",
+      },
+      {
+        word: "Oriental Mini",
+      },
+      {
+        word: "Oriental Shorthair",
+      },
+      {
+        word: "Oriental Stage Dragon",
+      },
+      {
+        word: "Orion",
+      },
+      {
+        word: "Osiris",
+      },
+      {
+        word: "Ovis Armentum",
+      },
+      {
+        word: "Painted Panther",
+      },
+      {
+        word: "Paix Evol",
+      },
+      {
+        word: "Palace Cat",
+      },
+      {
+        word: "Pale Panther",
+      },
+      {
+        word: "Pandora",
+      },
+      {
+        word: "Pandora's Box",
+      },
+      {
+        word: "Pandora's Gold",
+      },
+      {
+        word: "Parisian Art Cat",
+      },
+      {
+        word: "Persian",
+      },
+      {
+        word: "Peterbald",
+      },
+      {
+        word: "Phantom Cat",
+      },
+      {
+        word: "Pinekin",
+      },
+      {
+        word: "Pixiedust",
+      },
+      {
+        word: "Pocket Cat",
+      },
+      {
+        word: "Pointed Forestcat",
+      },
+      {
+        word: "Prismatic Lynx",
+      },
+      {
+        word: "Promise",
+      },
+      {
+        word: "Puffball",
+      },
+      {
+        word: "Puffy Munchkin",
+      },
+      {
+        word: "Purrple",
+      },
+      {
+        word: "Querida",
+      },
+      {
+        word: "RagaMuffin",
+      },
+      {
+        word: "Ragbob",
+      },
+      {
+        word: "Ragdoll",
+      },
+      {
+        word: "Rainbow Lynx",
+      },
+      {
+        word: "Rajah",
+      },
+      {
+        word: "Ramya",
+      },
+      {
+        word: "Recincho",
+      },
+      {
+        word: "Red Russian",
+      },
+      {
+        word: "Rex-Fold",
+      },
+      {
+        word: "Rhapsody",
+      },
+      {
+        word: "Ridet",
+      },
+      {
+        word: "Risadinha",
+      },
+      {
+        word: "Royal Crown",
+      },
+      {
+        word: "Russian Blue",
+      },
+      {
+        word: "Russian Shorthair",
+      },
+      {
+        word: "Salali",
+      },
+      {
+        word: "Saqqara Mau",
+      },
+      {
+        word: "Satin Celeste",
+      },
+      {
+        word: "Savannah",
+      },
+      {
+        word: "Scandinavian Curl",
+      },
+      {
+        word: "Scottish Fold",
+      },
+      {
+        word: "Scottish Wildcat",
+      },
+      {
+        word: "Seille",
+      },
+      {
+        word: "Selendis",
+      },
+      {
+        word: "Selkirk Rex",
+      },
+      {
+        word: "Senda",
+      },
+      {
+        word: "Seraphiner",
+      },
+      {
+        word: "Shaded Fold",
+      },
+      {
+        word: "Shapoo",
+      },
+      {
+        word: "Shiranami",
+      },
+      {
+        word: "Shmoe",
+      },
+      {
+        word: "Siamese",
+      },
+      {
+        word: "Siberian",
+      },
+      {
+        word: "Sidhe Cat",
+      },
+      {
+        word: "Silky Bob",
+      },
+      {
+        word: "Silver Rag",
+      },
+      {
+        word: "Silverbirman",
+      },
+      {
+        word: "Silvertroll",
+      },
+      {
+        word: "Singapura",
+      },
+      {
+        word: "Skybolt",
+      },
+      {
+        word: "Skyborn Mink",
+      },
+      {
+        word: "Slinky Mink",
+      },
+      {
+        word: "Smoky Joe",
+      },
+      {
+        word: "Snarfkat",
+      },
+      {
+        word: "Snowdrop",
+      },
+      {
+        word: "Snowrose",
+      },
+      {
+        word: "Snowshoe",
+      },
+      {
+        word: "Sokoke",
+      },
+      {
+        word: "Soluciam Silk",
+      },
+      {
+        word: "Somali",
+      },
+      {
+        word: "Soul Dream",
+      },
+      {
+        word: "Sphynx",
+      },
+      {
+        word: "Spotted Pfepfer",
+      },
+      {
+        word: "Spyhops",
+      },
+      {
+        word: "Stage Dragon",
+      },
+      {
+        word: "Starburst",
+      },
+      {
+        word: "Starlight",
+      },
+      {
+        word: "Striving",
+      },
+      {
+        word: "Swelongy",
+      },
+      {
+        word: "Sycamore",
+      },
+      {
+        word: "Tabby Joe",
+      },
+      {
+        word: "Taikin",
+      },
+      {
+        word: "Talmund",
+      },
+      {
+        word: "Tamri",
+      },
+      {
+        word: "Tansy Silkie",
+      },
+      {
+        word: "Tasmanian Pygmy Viper",
+      },
+      {
+        word: "Texas Tumbleweed",
+      },
+      {
+        word: "Thai",
+      },
+      {
+        word: "Tibetan",
+      },
+      {
+        word: "Ticked European Shorthair",
+      },
+      {
+        word: "Tierkreis",
+      },
+      {
+        word: "Tiffany",
+      },
+      {
+        word: "Tiger Lily",
+      },
+      {
+        word: "Tigerette",
+      },
+      {
+        word: "Tigraunda",
+      },
+      {
+        word: "Tilly Tabby",
+      },
+      {
+        word: "Tinted Coon",
+      },
+      {
+        word: "Tinted European Shorthair",
+      },
+      {
+        word: "Tirkner",
+      },
+      {
+        word: "Titan",
+      },
+      {
+        word: "Tonkinese",
+      },
+      {
+        word: "Tortie Angel",
+      },
+      {
+        word: "Toveurox",
+      },
+      {
+        word: "Tribble",
+      },
+      {
+        word: "Turkish Angora",
+      },
+      {
+        word: "Turkish Coon",
+      },
+      {
+        word: "Turkish Forestcat",
+      },
+      {
+        word: "Turkish Van",
+      },
+      {
+        word: "Tybee Cat",
+      },
+      {
+        word: "Ukrainian Levkoy",
+      },
+      {
+        word: "Ultra Curl",
+      },
+      {
+        word: "Veranda",
+      },
+      {
+        word: "Veranda Ottercat",
+      },
+      {
+        word: "Weird Ones",
+      },
+      {
+        word: "West End Blues",
+      },
+      {
+        word: "Whimsy Querl",
+      },
+      {
+        word: "Winter Ottercat",
+      },
+      {
+        word: "Wollygog",
+      },
+      {
+        word: "Wondercat",
+      },
+      {
+        word: "Xyndarian",
+      },
+      {
+        word: "Yakima Bobtail",
+      },
+      {
+        word: "York Chocolate",
+      },
+      {
+        word: "Zerdali Painted Cat",
+      },
+      {
+        word: "Zilberling",
+      },
+      {
+        word: "Zubumafu",
+      },
+      {
+        word: "Zuntichizxa",
+      },
+      {
+        word: "Zurelli Star",
+      }
+]
