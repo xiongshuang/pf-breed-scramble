f080bb9bc89dcdd21bbff59e156001f53ac403df
bjorn <156990105+xiongshuang@users.noreply.github.com>
2025-01-19 00:54:09 -0600

    added chant's new version

--- old scripts/script.js
+++ old scripts/script.js
@@ -2,7 +2,6 @@
 
 const wordText = document.querySelector(".word"),
 hintText = document.querySelector(".hint span"),
-timeText = document.querySelector(".time b"),
 inputField = document.querySelector("input"),
 refreshBtn = document.querySelector(".refresh-word"),
 checkBtn = document.querySelector(".check-word");
